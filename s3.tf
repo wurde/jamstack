83cf784f2f530cce86b73884b96cca1a6c7afda2
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-05-10 21:29:12 -0500

    set index html

--- s3.tf
+++ s3.tf
@@ -8,4 +8,11 @@ resource "aws_s3_bucket" "domain" {
   # The canned ACL to apply. Defaults to "private".
   # https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl
   acl = "public-read"
+
+  # Configure your bucket as a static website. It'll be available
+  # at the AWS Region-specific website endpoint of the bucket.
+  #   http://bucket-name.s3-website-Region.amazonaws.com
+  website {
+    index_document = "index.html"
+  }
 }

8735592ecc6063ee55db98b3dea0a1d28de5632c
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-05-10 21:25:27 -0500

    set public ACL

--- s3.tf
+++ s3.tf
@@ -4,4 +4,8 @@
 resource "aws_s3_bucket" "domain" {
   # The name of the bucket.
   bucket = var.s3_bucket_domain == "" ? var.domain : var.s3_bucket_domain
+
+  # The canned ACL to apply. Defaults to "private".
+  # https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl
+  acl = "public-read"
 }

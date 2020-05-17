dcb862402cc617392cff40d4637af125a06c11c4
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-05-17 15:37:11 -0500

    add password policy

--- cognito.tf
+++ cognito.tf
@@ -10,6 +10,41 @@ resource "aws_cognito_user_pool" "main" {
     case_sensitive = false
   }
 
+  # (Optional) - A container with the schema attributes of a
+  #   user pool. Schema attributes from the standard attribute
+  #   set only need to be specified if they are different from
+  #   the default configuration. Maximum of 50 attributes.
+  schema {
+    # (Required) - The name of the attribute.
+    name = "email"
+
+    # (Required) - The attribute data type. (Boolean, Number, String, or DateTime)
+    attribute_data_type = "String"
+
+    # (Optional) - Specifies whether a user pool attribute is required.
+    #   If the attribute is required and the user does not provide a value,
+    #   registration or sign-in will fail.
+    required = true
+  }
+
   # (Optional) - Information about the password policy.
-  password_policy {}
+  password_policy {
+    # The minimum length of the password.
+    minimum_length = 8
+
+    # Require one lowercase letter.
+    require_lowercase = true
+
+    # Require at least one number.
+    require_numbers = true
+
+    # Require at least one symbol.
+    require_symbols = true
+
+    # Require at least one uppercase letter.
+    require_uppercase = true
+
+    # Number of days a temporary password is valid.
+    temporary_password_validity_days = 7
+  }
 }

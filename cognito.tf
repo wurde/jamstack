6bfaad2b31c86627953f9f19fd5bef93562ce849
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-05-17 14:51:25 -0500

    toggle off case-sensitivity on usernames

--- cognito.tf
+++ cognito.tf
@@ -3,4 +3,13 @@
 resource "aws_cognito_user_pool" "main" {
   # (Required) The name of the user pool.
   name = "main"
+
+  # (Optional) The Username Configuration.
+  username_configuration {
+    # (Required) - Enable username case sensitivity.
+    case_sensitive = false
+  }
+
+  # (Optional) - Information about the password policy.
+  password_policy {}
 }

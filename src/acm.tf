89724ac59d681a6b7a809018b52878f0cbabf157
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-06-15 01:44:29 -0500

    ssl

--- src/acm.tf
+++ src/acm.tf
@@ -15,6 +15,6 @@ resource "aws_acm_certificate" "ssl" {
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
+  certificate_arn         = aws_acm_certificate.ssl.arn
   validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
 }

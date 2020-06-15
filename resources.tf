71d3bd758aff45b81183bc6c3c51f79897b6c13e
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-06-14 21:16:49 -0500

    format

--- resources.tf
+++ resources.tf
@@ -3,12 +3,11 @@
 module "resources" {
   source = "./src"
 
-  dist_dir       = var.dist_dir
-  domain         = var.domain
-  domain_comment = var.domain_comment
-  subdomain_www  = var.subdomain_www
-
-  s3_bucket_domain         = var.s3_bucket_domain
-  s3_bucket_subdomain_www  = var.s3_bucket_subdomain_www
-  country_blacklist        = var.country_blacklist
+  dist_dir                = var.dist_dir
+  domain                  = var.domain
+  domain_comment          = var.domain_comment
+  subdomain_www           = var.subdomain_www
+  s3_bucket_domain        = var.s3_bucket_domain
+  s3_bucket_subdomain_www = var.s3_bucket_subdomain_www
+  country_blacklist       = var.country_blacklist
 }

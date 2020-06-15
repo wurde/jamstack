db3ac5693743a16304dffc029bd8344c82642219
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-06-15 01:43:41 -0500

    ssl

--- src/route53.tf
+++ src/route53.tf
@@ -62,8 +62,8 @@ resource "aws_route53_record" "AAAA" {
 
 resource "aws_route53_record" "cert_validation" {
   zone_id = aws_route53_zone.domain.zone_id
-  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
-  records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
+  name    = aws_acm_certificate.ssl.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.ssl.domain_validation_options.0.resource_record_type
+  records = [aws_acm_certificate.ssl.domain_validation_options.0.resource_record_value]
   ttl     = 60
 }

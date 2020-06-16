9646943d4afd643531e5d371e8a3f1b24e46c073
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-06-16 01:09:44 -0500

    uncomment route53

--- src/route53.tf
+++ src/route53.tf
@@ -1,69 +1,69 @@
-# # Define Route53 resources.
-# # https://aws.amazon.com/route53
+# Define Route53 resources.
+# https://aws.amazon.com/route53
 
-# # A hosted zone is a container that holds information about how you want to route
-# # traffic for a domain, such as example.com, and its subdomains.
-# # https://www.terraform.io/docs/providers/aws/r/route53_zone.html
-# resource "aws_route53_zone" "domain" {
-#   # Name of the hosted zone.
-#   name = var.domain
+# A hosted zone is a container that holds information about how you want to route
+# traffic for a domain, such as example.com, and its subdomains.
+# https://www.terraform.io/docs/providers/aws/r/route53_zone.html
+resource "aws_route53_zone" "domain" {
+  # Name of the hosted zone.
+  name = var.domain
 
-#   # A comment for the hosted zone. Defaults to 'Managed by Terraform'.
-#   comment = var.domain_comment
+  # A comment for the hosted zone. Defaults to 'Managed by Terraform'.
+  comment = var.domain_comment
 
-#   # Destroy all records in the zone when destroying the zone.
-#   force_destroy = true
-# }
+  # Destroy all records in the zone when destroying the zone.
+  force_destroy = true
+}
 
-# # If IPv6 is enabled for CloudFront, you'll need to create two alias records for
-# # the distribution, one to route IPv4 traffic to the distribution, and one to
-# # route IPv6 traffic.
+# If IPv6 is enabled for CloudFront, you'll need to create two alias records for
+# the distribution, one to route IPv4 traffic to the distribution, and one to
+# route IPv6 traffic.
 
-# # https://www.terraform.io/docs/providers/aws/r/route53_record.html
-# resource "aws_route53_record" "A" {
-#   # The ID of the hosted zone to contain this record.
-#   zone_id = aws_route53_zone.domain.zone_id
+# https://www.terraform.io/docs/providers/aws/r/route53_record.html
+resource "aws_route53_record" "A" {
+  # The ID of the hosted zone to contain this record.
+  zone_id = aws_route53_zone.domain.zone_id
 
-#   # The name of the record.
-#   name = var.domain
+  # The name of the record.
+  name = var.domain
 
-#   # The record type. Valid values are A, AAAA, CAA, CNAME, MX, NAPTR,
-#   #   NS, PTR, SOA, SPF, SRV and TXT.
-#   type = "A"
+  # The record type. Valid values are A, AAAA, CAA, CNAME, MX, NAPTR,
+  #   NS, PTR, SOA, SPF, SRV and TXT.
+  type = "A"
 
-#   alias {
-#     # DNS domain name for a CloudFront distribution, S3 bucket, ELB,
-#     #   or another resource record set in this hosted zone.
-#     name = aws_cloudfront_distribution.cdn.domain_name
+  alias {
+    # DNS domain name for a CloudFront distribution, S3 bucket, ELB,
+    #   or another resource record set in this hosted zone.
+    name = aws_cloudfront_distribution.cdn.domain_name
 
-#     # Hosted zone ID for a CloudFront distribution, S3 bucket, ELB, or
-#     #   Route 53 hosted zone.
-#     zone_id = aws_cloudfront_distribution.cdn.hosted_zone_id
+    # Hosted zone ID for a CloudFront distribution, S3 bucket, ELB, or
+    #   Route 53 hosted zone.
+    zone_id = aws_cloudfront_distribution.cdn.hosted_zone_id
 
-#     # Set to true if you want Route 53 to determine whether to respond
-#     #   to DNS queries using this resource record set by checking the health of
-#     #   the resource record set.
-#     evaluate_target_health = false
-#   }
-# }
+    # Set to true if you want Route 53 to determine whether to respond
+    #   to DNS queries using this resource record set by checking the health of
+    #   the resource record set.
+    evaluate_target_health = false
+  }
+}
 
-# resource "aws_route53_record" "AAAA" {
-#   zone_id = aws_route53_zone.domain.zone_id
-#   name    = var.domain
-#   type    = "AAAA"
+resource "aws_route53_record" "AAAA" {
+  zone_id = aws_route53_zone.domain.zone_id
+  name    = var.domain
+  type    = "AAAA"
 
-#   alias {
-#     name    = aws_cloudfront_distribution.cdn.domain_name
-#     zone_id = aws_cloudfront_distribution.cdn.hosted_zone_id
+  alias {
+    name    = aws_cloudfront_distribution.cdn.domain_name
+    zone_id = aws_cloudfront_distribution.cdn.hosted_zone_id
 
-#     evaluate_target_health = false
-#   }
-# }
+    evaluate_target_health = false
+  }
+}
 
-# resource "aws_route53_record" "cert_validation" {
-#   zone_id = aws_route53_zone.domain.zone_id
-#   name    = aws_acm_certificate.ssl.domain_validation_options.0.resource_record_name
-#   type    = aws_acm_certificate.ssl.domain_validation_options.0.resource_record_type
-#   records = [aws_acm_certificate.ssl.domain_validation_options.0.resource_record_value]
-#   ttl     = 60
-# }
+resource "aws_route53_record" "cert_validation" {
+  zone_id = aws_route53_zone.domain.zone_id
+  name    = aws_acm_certificate.ssl.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.ssl.domain_validation_options.0.resource_record_type
+  records = [aws_acm_certificate.ssl.domain_validation_options.0.resource_record_value]
+  ttl     = 60
+}

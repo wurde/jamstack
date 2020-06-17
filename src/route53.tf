2e18b80f69faa0d79bbaef459e5841a3323032c7
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-06-17 04:11:39 -0500

    multiple validation certs

--- src/route53.tf
+++ src/route53.tf
@@ -68,9 +68,11 @@ resource "aws_route53_record" "CNAME" {
 }
 
 resource "aws_route53_record" "cert_validation" {
+  count = length(var.aws_acm_certificate.ssl.domain_validation_options)
+
   zone_id = aws_route53_zone.domain.zone_id
-  name    = aws_acm_certificate.ssl.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.ssl.domain_validation_options.0.resource_record_type
-  records = [aws_acm_certificate.ssl.domain_validation_options.0.resource_record_value]
+  name    = aws_acm_certificate.ssl.domain_validation_options[count.index].resource_record_name
+  type    = aws_acm_certificate.ssl.domain_validation_options[count.index].resource_record_type
+  records = [aws_acm_certificate.ssl.domain_validation_options[count.index].resource_record_value]
   ttl     = 60
 }

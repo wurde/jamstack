a81ee1245c9f7b79d761993eaf9ad9e768ad0881
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-06-17 02:32:24 -0500

    test toggle

--- resources.tf
+++ resources.tf
@@ -5,9 +5,9 @@ module "resources" {
 
   dist_dir                = var.dist_dir
   domain                  = var.domain
-  domain_comment          = var.domain_comment
   subdomain_www           = var.subdomain_www
   s3_bucket_domain        = var.s3_bucket_domain
   s3_bucket_subdomain_www = var.s3_bucket_subdomain_www
   country_blacklist       = var.country_blacklist
+  toggle                  = var.toggle
 }

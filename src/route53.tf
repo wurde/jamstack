62e5e0d0cdb8ad781acbc7ea5bbe0e66cb3cfc84
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-06-23 06:39:49 -0500

    for_each again

--- src/route53.tf
+++ src/route53.tf
@@ -68,11 +68,11 @@ resource "aws_route53_record" "CNAME" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  count = length(aws_acm_certificate.ssl.domain_validation_options)
+  for_each = aws_acm_certificate.ssl.domain_validation_options
 
   zone_id = aws_route53_zone.domain.zone_id
-  name    = aws_acm_certificate.ssl.domain_validation_options[count.index].resource_record_name
-  type    = aws_acm_certificate.ssl.domain_validation_options[count.index].resource_record_type
-  records = [aws_acm_certificate.ssl.domain_validation_options[count.index].resource_record_value]
+  name    = each.value.resource_record_name
+  type    = each.value.resource_record_type
+  records = [each.value.resource_record_value]
   ttl     = 60
 }

ef07d048bba210b78a719543833a4429baefeceb
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-06-23 06:38:34 -0500

    undo

--- src/route53.tf
+++ src/route53.tf
@@ -68,12 +68,11 @@ resource "aws_route53_record" "CNAME" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  for_each = { for option in aws_acm_certificate.ssl.domain_validation_options: option.resource_record_name => option }
-  # for_each = toset(aws_acm_certificate.ssl.domain_validation_options)
+  count = length(aws_acm_certificate.ssl.domain_validation_options)
 
   zone_id = aws_route53_zone.domain.zone_id
-  name    = each.value.resource_record_name
-  type    = each.value.resource_record_type
-  records = [each.value.resource_record_value]
+  name    = aws_acm_certificate.ssl.domain_validation_options[count.index].resource_record_name
+  type    = aws_acm_certificate.ssl.domain_validation_options[count.index].resource_record_type
+  records = [aws_acm_certificate.ssl.domain_validation_options[count.index].resource_record_value]
   ttl     = 60
 }
